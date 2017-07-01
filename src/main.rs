7fec21076a15e27b3034297b6da0172cbd1697b3
Justin Cullen <jcullen@alum.mit.edu>
2017-07-01 15:21:57 -0400

    More.

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,9 @@ extern crate rayon;
 
 use std::io;
 use std::cmp::max;
-use rand::Rng;
-use std::hash::{Hash,Hasher};
+use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
-use rand::XorShiftRng;
+use rand::{Rng, XorShiftRng};
 use std::fmt;
 use std::mem::transmute;
 use std::marker::PhantomData;
@@ -20,7 +19,7 @@ use rayon::prelude::*;
 const HEIGHT: usize = 6;
 const WIDTH: usize = 7;
 const NEEDED: usize = 4;
-const SEARCH_DEPTH: usize = 5;
+const SEARCH_DEPTH: usize = 1;
 
 #[derive(Debug, Clone, Copy, PartialEq, Hash)]
 enum Color {
@@ -40,8 +39,8 @@ impl Color {
     fn show(&self) -> &str {
         use Color::*;
         match *self {
-            R => "Red",
-            B => "Black",
+            R => "X",
+            B => "O",
         }
     }
 
@@ -68,14 +67,12 @@ impl Slot {
 
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match *self {
-                Color::R => "R",
-                Color::B => "B",
-            }
-        )
+        write!(f,
+               "{}",
+               match *self {
+                   Color::R => "X",
+                   Color::B => "O",
+               })
     }
 }
 
@@ -151,7 +148,7 @@ impl ValidMove {
     }
 }
 
-impl <'gs> ValidMoveMut<'gs> {
+impl<'gs> ValidMoveMut<'gs> {
     fn apply(self) -> (usize) {
         use Slot::*;
         let b = self.valid_for;
@@ -194,10 +191,6 @@ impl Board<Clean> {
     fn possible_moves(&self, color: Color) -> Vec<ValidMove> {
         let mut moves = vec![];
 
-        if self.winner().is_some() {
-            return moves;
-        }
-
         for j in 0..WIDTH {
             if let Slot::Empty = self.get(HEIGHT - 1, j) {
                 moves.push(ValidMove {
@@ -218,8 +211,7 @@ impl Board<Clean> {
         for j in 0..HEIGHT - (NEEDED - 1) {
             for i in 0..WIDTH {
                 if get(i, j) == color && get(i, j + 1) == color && get(i, j + 2) == color &&
-                    get(i, j + 3) == color
-                {
+                   get(i, j + 3) == color {
                     return true;
                 }
             }
@@ -228,8 +220,7 @@ impl Board<Clean> {
         for i in 0..WIDTH - (NEEDED - 1) {
             for j in 0..HEIGHT {
                 if get(i, j) == color && get(i + 1, j) == color && get(i + 2, j) == color &&
-                    get(i + 3, j) == color
-                {
+                   get(i + 3, j) == color {
                     return true;
                 }
             }
@@ -239,8 +230,7 @@ impl Board<Clean> {
         for i in 3..WIDTH {
             for j in 0..HEIGHT - (NEEDED - 1) {
                 if get(i, j) == color && get(i - 1, j + 1) == color &&
-                    get(i - 2, j + 2) == color && get(i - 3, j + 3) == color
-                {
+                   get(i - 2, j + 2) == color && get(i - 3, j + 3) == color {
                     return true;
                 }
             }
@@ -249,8 +239,7 @@ impl Board<Clean> {
         for i in 3..WIDTH {
             for j in 3..HEIGHT {
                 if get(i, j) == color && get(i - 1, j - 1) == color &&
-                    get(i - 2, j - 2) == color && get(i - 3, j - 3) == color
-                {
+                   get(i - 2, j - 2) == color && get(i - 3, j - 3) == color {
                     return true;
                 }
             }
@@ -314,29 +303,23 @@ impl Board<Clean> {
     }
 
     fn try_move_mut(&mut self, n: usize, c: Color) -> Option<usize> {
-        self.verify_move_mut(n, c).map(|m| {
-            m.apply()
-        })
+        self.verify_move_mut(n, c).map(|m| m.apply())
     }
 
     fn try_moves<I>(&mut self, moves: I) -> Vec<Option<usize>>
-    where
-        I: Iterator<Item = (usize, Color)>,
+        where I: Iterator<Item = (usize, Color)>
     {
-        moves
-            .map(move |(col, color)| self.try_move(col, color))
+        moves.map(move |(col, color)| self.try_move(col, color))
             .collect()
     }
 
     fn minimax(&self, to_act: Color) -> (i32, usize) {
-        let node = Node {
-            game: Game {
-                state: self.clone(),
-                to_act: to_act,
-                ref_color: to_act,
-            },
-            preceding_move: None,
+        let game = Game {
+            state: self.clone(),
+            to_act: to_act,
+            ref_color: to_act,
         };
+        let mut node = Node::new(game);
         node.negamax_ab(0, i32::min_value(), i32::max_value())
     }
 }
@@ -359,18 +342,18 @@ impl<T> Board<T> {
 
 impl<T> fmt::Display for Board<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let dashes: String = (0..WIDTH).map(|_| "-").collect();
+        let dashes: String = (0..WIDTH * 3).map(|_| "-").collect();
         for row in self.board.iter().rev() {
             write!(f, "|")?;
             for slot in row.iter() {
-                write!(f, "{}", slot)?;
+                write!(f, " {} ", slot)?;
             }
             writeln!(f, "|")?;
         }
         writeln!(f, "|{}|", dashes.as_str())?;
         write!(f, " ")?;
         for i in 0..WIDTH {
-            write!(f, "{}", i + 1)?;
+            write!(f, " {} ", i + 1)?;
         }
         writeln!(f, " ")
     }
@@ -383,29 +366,38 @@ struct Game {
     ref_color: Color,
 }
 
-#[derive(Hash)]
+#[derive(Clone)]
 struct Node {
     game: Game,
     preceding_move: Option<usize>,
+    rng: XorShiftRng,
 }
 
 impl Node {
-    fn negamax(&self, depth: usize) -> (i32, usize) {
-        let game = &self.game;
+    fn new_seeded(g: Game, seed: [u32; 4]) -> Self {
+        let rng: XorShiftRng = rand::SeedableRng::from_seed(seed);
+        Node {
+            game: g,
+            preceding_move: None,
+            rng: rng,
+        }
+    }
 
+    fn new(g: Game) -> Self {
+        let seed = rand::random::<[u32; 4]>();
+        Self::new_seeded(g, seed)
+    }
 
-        let nexts = game.possibilities();
+    fn negamax(&mut self, depth: usize) -> (i32, usize) {
+        let nexts = self.possibilities();
         if depth > SEARCH_DEPTH || nexts.is_empty() {
             assert!(!self.preceding_move.is_none());
-            return (
-                game.eval() * game.color_weight(),
-                self.preceding_move.unwrap(),
-            );
+            return (self.heuristic() * self.game.color_weight(self.game.to_act),
+                    self.preceding_move.unwrap());
         }
 
-        nexts
-            .par_iter()
-            .map(|node| {
+        nexts.into_par_iter()
+            .map(|mut node| {
                 let (s, m) = node.negamax(depth + 1);
                 (-s, m)
             })
@@ -414,23 +406,21 @@ impl Node {
 
 
     }
-    fn negamax_ab(&self, depth: usize, alpha: i32, beta: i32) -> (i32, usize) {
-        let game = &self.game;
+
+    fn negamax_ab(&mut self, depth: usize, alpha: i32, beta: i32) -> (i32, usize) {
 
 
-        let nexts = game.possibilities();
+        let nexts = self.possibilities();
         if depth > SEARCH_DEPTH || nexts.is_empty() {
             assert!(!self.preceding_move.is_none());
-            return (
-                game.eval() * game.color_weight(),
-                self.preceding_move.unwrap(),
-            );
+            return (self.heuristic() * self.game.color_weight(self.game.to_act),
+                    self.preceding_move.unwrap());
         }
 
         let mut best = i32::min_value();
         let mut best_move = None;
         let mut alpha = alpha;
-        for node in nexts {
+        for mut node in nexts {
             let (s, m) = node.negamax_ab(depth + 1, -beta, -alpha);
             let s = -s;
             if s >= best {
@@ -440,11 +430,67 @@ impl Node {
 
             alpha = max(alpha, best);
             if alpha >= beta {
-                break
+                break;
             }
         }
 
         (best, best_move.unwrap())
+    }
+
+    fn random_outcome(&mut self) -> Option<Color> {
+        // let seed = calculate_hash(self);
+
+        let mut game = (*self).game.clone();
+        while !game.state.has_winner() {
+            let moves: Vec<usize> =
+                game.state.possible_moves(game.to_act).into_iter().map(|m| m.index()).collect();
+            let next_move: Option<&usize> = self.rng.choose(moves.as_ref());
+            if next_move.is_none() {
+                return None;
+            };
+            let index = *next_move.unwrap();
+            game.state.try_move_mut(index, game.to_act);
+            game.to_act = game.to_act.flip();
+        }
+        game.state.winner()
+    }
+
+    fn monte_carlo(&mut self, trials: u32) -> u32 {
+        let ref_color = self.game.ref_color;
+        (0..trials)
+            .flat_map(|_| self.random_outcome())
+            .filter(move |c| *c == ref_color)
+            .map(|_| 1)
+            .sum()
+    }
+
+    fn heuristic(&mut self) -> i32 {
+        if let Some(c) = self.game.state.winner() {
+            if c == self.game.ref_color {
+                return i32::max_value();
+            } else {
+                return i32::min_value();
+            }
+        } else {
+            self.monte_carlo(100) as i32
+        }
+    }
+
+    fn possibilities(&self) -> Vec<Node> {
+        let moves = self.game.state.possible_moves(self.game.to_act);
+        moves.into_iter()
+            .map(move |m| {
+                Node {
+                    rng: self.rng.clone(),
+                    preceding_move: Some(m.index()),
+                    game: Game {
+                        state: m.apply().0,
+                        to_act: self.game.to_act.flip(),
+                        ref_color: self.game.ref_color,
+                    },
+                }
+            })
+            .collect()
     }
 }
 
@@ -455,69 +501,16 @@ fn calculate_hash<T: Hash>(t: &T) -> [u32; 4] {
     let low: u32 = x as u32;
     let high: u32 = (x >> 32) as u32;
     [low, high, low, high]
-        
 }
 
 impl Game {
-    fn eval(&self) -> i32 {
-        if let Some(c) = self.state.winner() {
-            if c == self.ref_color {
-                return i32::max_value();
-            } else {
-                return i32::min_value();
-            }
-        } else {
-            self.monte_carlo(100) as i32
-        }
-    }
-
-    fn random_outcome(&self) -> Option<Color> {
-        // let seed = calculate_hash(self);
-        let seed = rand::random::<[u32; 4]>();
-        let mut rng : XorShiftRng = rand::SeedableRng::from_seed(seed);
-        
-        let mut game = (*self).clone();
-        while !game.state.has_winner() {
-            let moves : Vec<usize> = game.state.possible_moves(game.to_act).into_iter().map(|m| m.index()).collect();
-            let next_move : Option<&usize> = rng.choose(moves.as_ref());
-            if next_move.is_none() { return None };
-            let index = *next_move.unwrap();
-            game.state.try_move_mut(index, game.to_act);
-            game.to_act = game.to_act.flip();
-        }
-       game.state.winner()
-    }
-
-    fn monte_carlo(&self, trials: u32)  -> u32 {
-        (0..trials).flat_map(|_| {
-            self.random_outcome()
-        }).filter(|c| *c == self.ref_color).map(|_| 1).sum()
-    }
-
-    fn color_weight(&self) -> i32 {
-        if self.to_act == self.ref_color { 1 } else { -1 }
+    fn color_weight(&self, c: Color) -> i32 {
+        if c == self.ref_color { 1 } else { -1 }
     }
 
 
     fn ref_color_to_act(&self) -> bool {
         self.ref_color == self.to_act
-    }
-
-    fn possibilities(&self) -> Vec<Node> {
-        let moves = self.state.possible_moves(self.to_act);
-        moves
-            .into_iter()
-            .map(move |m| {
-                Node {
-                    preceding_move: Some(m.index()),
-                    game: Game {
-                        state: m.apply().0,
-                        to_act: self.to_act.flip(),
-                        ref_color: self.ref_color,
-                    },
-                }
-            })
-            .collect()
     }
 }
 
@@ -545,66 +538,100 @@ fn test() {
     // println!("{}", board.transposed());
 }
 
-fn get_human_move(board: &Board<Clean>, color: Color) -> usize {
-    println!("{}'s move! Here is the board:\n", color.show());
-    println!("{}", board);
 
-    loop {
-        println!("What is your move?");
-        println!("Enter a number between {} and {}", 1, WIDTH);
-        let mut choice = String::new();
-        io::stdin().read_line(&mut choice).expect(
-            "Failed to read line... something is mad broke.",
-        );
+trait Player {
+    fn choose_move(&mut self, board: &Board<Clean>, color: Color) -> usize;
+    fn display_name(&self) -> &str;
+}
 
-        let choice: usize = match choice.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+struct HumanPlayer {
+    name: String,
+}
 
-        if !(1 <= choice && choice <= WIDTH) {
-            println!("Try a better number...");
-            continue;
+impl HumanPlayer {
+    fn new(name: String) -> Self {
+        HumanPlayer {
+            name: name,
         }
-
-        if board.clone().verify_move(choice - 1, color).is_none() {
-            println!("Invalid move..");
-            continue;
-        }
-
-        return choice - 1;
     }
 }
 
-fn get_computer_move(board: &Board<Clean>, color: Color) -> usize {
-    println!("Computer is thinking.....");
-    let (s, m) = board.minimax(color);
-    println!("score: {}", s);
-    m
+impl Player for HumanPlayer {
+    fn display_name(&self) -> &str {
+        "You"
+    }
+    fn choose_move(&mut self, board: &Board<Clean>, color: Color) -> usize {
+        println!("{}'s move.", color.show());
+
+        loop {
+            println!("What is your move?");
+            print!("Enter a number between {} and {}:", 1, WIDTH);
+            let mut choice = String::new();
+            io::stdin().read_line(&mut choice).expect("Failed to read line... something is mad broke.");
+            println!("");
+
+            let choice: usize = match choice.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+            if !(1 <= choice && choice <= WIDTH) {
+                println!("Try a better number...");
+                continue;
+            }
+
+            if board.clone().verify_move(choice - 1, color).is_none() {
+                println!("Invalid move..");
+                continue;
+            }
+
+            return choice - 1;
+        }
+    }
+}
+struct AIPlayer {}
+
+impl Player for AIPlayer {
+    fn display_name(&self) -> &str {
+        "Computer"
+    }
+
+    fn choose_move(&mut self, board: &Board<Clean>, color: Color) -> usize {
+        println!("Computer is thinking.....");
+        let (s, m) = board.minimax(color);
+        println!("score: {}", s);
+        m
+    }
 }
 
-fn run_game() {
+impl AIPlayer {
+    fn new() -> Self {
+        AIPlayer {}
+    }
+}
+
+fn run_game(p1: &mut Player, p2: &mut Player) {
     use Color::*;
-    println!("Connect four time..");
-    println!("You are red, and the computer is black..");
+    println!("CONNECT FOUR");
+    println!("Player 1, {}, is {}'s.", p1.display_name(), R.show());
+    println!("Player 2, {}, is {}'s.", p2.display_name(), B.show());
     println!("Flipping to see who starts...");
 
     let mut to_act = Color::random();
-    println!(
-        "{} goes first!",
-        to_act.show()
-    );
+    println!("{} goes first!", to_act.show());
 
     let mut board = Board::new();
     while !board.has_winner() {
         let cloned_board = board.clone();
+        println!("{}", board);
         match to_act {
             R => {
-                let hmove = get_human_move(&cloned_board, R);
-                board.try_move(hmove, R);
+                let p1_move = (*p1).choose_move(&cloned_board, R);
+                board.try_move(p1_move, R);
             }
             B => {
-                board.try_move(get_computer_move(&cloned_board, B), B);
+                let p2_move = (*p2).choose_move(&cloned_board, B);
+                board.try_move(p2_move, B);
             }
         }
         to_act = to_act.flip();
@@ -615,5 +642,7 @@ fn run_game() {
 }
 
 fn main() {
-    run_game();
+    let mut human = HumanPlayer::new(String::from("Justin"));
+    let mut pc = AIPlayer::new();
+    run_game(&mut human, &mut pc);
 }
