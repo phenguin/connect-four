6db0e490878d322441ec3ce2b5e882874282a716
Justin Cullen <jcullen@alum.mit.edu>
2018-02-19 15:35:49 -0500

    Profiling.

--- src/bin/profile.rs
+++ src/bin/profile.rs
@@ -4,19 +4,24 @@ extern crate gameai;
 extern crate cpuprofiler;
 
 use cpuprofiler::PROFILER;
-use gameai::strategies::negamax::{Negamax, NegamaxParams};
 use gameai::strategies::Strategy;
-use gameai::game::Game;
-use gameai::game::connectfour::{ConnectFour, Color};
+use gameai::game::trivial::*;
+use gameai::strategies::mcts_rayon;
 use gameai::*;
+use gameai::game::Game;
 
 fn do_profile() {
     PROFILER.lock().unwrap().start("./negamax.profile");
-    let mut strategy = Negamax::create(NegamaxParams {
-        max_depth: 5,
-        trials: 100,
+    let mut strategy = mcts_rayon::MCTS::create(mcts_rayon::MCTSParams {
+        workers: 1,
+        worker_batch_size: 1,
+        merger_batch_size: 1,
+        min_flush_interval: 1,
+        merger_queue_bound: 1,
+        timeout: 1,
+        c: (2.0 as f64).sqrt(),
     });
-    debug(&strategy.decide(&ConnectFour::new(&Color::R)));
+    debug(&strategy.decide(&TrivialGame::new(&Player::A)));
     PROFILER.lock().unwrap().stop();
 }
 
